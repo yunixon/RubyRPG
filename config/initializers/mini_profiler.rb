ff712eb84b1c059b8ce471d99c9edf88f35b827c
Yuri Skurikhin <yunixon@gmail.com>
2015-09-11 08:08:33 +0300

    mini profiler only for dev env

--- config/initializers/mini_profiler.rb
+++ config/initializers/mini_profiler.rb
@@ -1,4 +1,11 @@
 # Fixes "Model 'Mini-profiler-resources' could not be found" bug when
 # in development mode using Rails Admin
-Rack::MiniProfiler.config.skip_paths ||= []
-Rack::MiniProfiler.config.skip_paths << '/admin'
+if Rails.env == 'development'
+  require 'rack-mini-profiler'
+
+  # initialization is skipped so trigger it
+  Rack::MiniProfilerRails.initialize!(Rails.application)
+  Rack::MiniProfiler.config.start_hidden = true
+  Rack::MiniProfiler.config.skip_paths ||= []
+  Rack::MiniProfiler.config.skip_paths << '/admin'
+end
