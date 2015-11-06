ebe5d9c0563060bd183b87817c1a8c978a17d50d
Yuri Skurikhin <yunixon@gmail.com>
2015-11-06 12:58:12 +0300

    Fix prod deploy, mail first step

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,13 @@ set :stage, :production
 set :rails_env, :production
 set :branch, :master
 
+role :app, %w{deploy@188.227.75.14}
+role :web, %w{deploy@188.227.75.14}
+
+set :sidekiq_role, :app
+set :sidekiq_config, "#{shared_path}/config/sidekiq.yml"
+set :sidekiq_env, :production
+
 set :deploy_to, "/home/deploy/apps/#{fetch(:application)}"
 
 server '188.227.75.14', user: 'deploy', roles: %w{web app db worker}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'example.com' }
+  config.action_mailer.default_options = { from: 'no-reply@example.com' }
 end
